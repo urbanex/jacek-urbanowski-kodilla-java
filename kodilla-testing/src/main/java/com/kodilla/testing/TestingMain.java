7652e07ad413e2abda55eea092fdf637de87287f
urbanex <urbanex89@gmail.com>
2017-09-07 18:10:04 +0200

    submodul 6.2 podejscie nr 1

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        System.out.println("Przykład z Kodilli");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("Test OK!");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println();
+        System.out.println("Zadanie - testowanie dodawania:");
+
+        int addResult = Calculator.add(7,5);
+
+        if (addResult == 12) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println();
+        System.out.println("Zadanie - testowanie odejmowania:");
+        int substractResult = Calculator.substract(15,2);
+
+        if (substractResult == 13) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Error!");
+        }
 
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int substract(int a, int b) {
+        return a - b;
+    }
+
+}
