11bcb8a2dc184ee0a2f1002a81359fc370ae7243
urbanex <urbanex89@gmail.com>
2017-09-23 00:11:56 +0200

    modul 8.2 zadanie podejscie 1

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/challenge/FirstChallenge.java
@@ -0,0 +1,37 @@
+package com.kodilla.exception.challenge;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Pamietaj cholero - nigdy nie dziel przez zero! ¯\\_(ツ)_/¯");
+
+        } finally {
+
+            System.out.println("End of program");
+
+        }
+
+    }
+
+}
