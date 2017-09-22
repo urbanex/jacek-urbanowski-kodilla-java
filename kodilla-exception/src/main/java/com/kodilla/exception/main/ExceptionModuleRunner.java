acf8a4d4f8e587e33a1ab24670b99bf5b2c3a89f
urbanex <urbanex89@gmail.com>
2017-09-23 01:04:44 +0200

    modul 8.2 i 8.3 przyklady

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,13 +1,43 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+
+import java.io.IOException;
 
 public class ExceptionModuleRunner {
 
     public static void main(String args[]) {
 
+        //moduł 8.1 przykład
+        /*FileReader fileReader = new FileReader();
+        fileReader.readFile();*/
+
+        //moduł 8.3
+        FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
+
+        try {
+            fileReaderWithoutHandling.readFile();
+        } catch (IOException e) {
+            System.out.println("Oh no!!!!!!! Sth went wrong ¯\\_(ツ)_/¯");
+        }
+
+        System.out.println();
+
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e) {
+            System.out.println("Oh no!!!!!!! Sth went wrong ¯\\_(ツ)_/¯");
+        }
+
+        System.out.println();
+
+        //8.3 zadanie
+
+
 
     }
 
