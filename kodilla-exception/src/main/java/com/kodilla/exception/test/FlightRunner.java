632e480db9a270a331fb5ec737a51b020a146aa6
urbanex <urbanex89@gmail.com>
2017-09-25 21:21:43 +0200

    modul 8.4 zadanie podejscie 2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class FlightRunner {
 
-    public boolean findFlght(Flight flight) throws RouteNotFoundException {
+    public boolean findFlight(Flight flight) throws RouteNotFoundException {
         String message = "there is no airport in ";
         Map<String, Boolean> map = new HashMap<>();
         map.put("Bratislava", true);
@@ -23,14 +23,22 @@ public class FlightRunner {
         map.put("Madrid", false);
         map.put("Lisbon", false);
 
-        if (map.containsKey(flight.getArrivalAirport()) && map.containsKey(flight.getDepartureAirport())) {
-            return map.get(flight.getArrivalAirport()) && map.get(flight.getDepartureAirport());
+        boolean mapContainsArrivalAirport = map.containsKey(flight.getArrivalAirport());
+        boolean mapContainsDepartureAirport = map.containsKey(flight.getDepartureAirport());
+
+        String arrivalAirport = flight.getArrivalAirport();
+        String departureAirport = flight.getDepartureAirport();
+
+        if (mapContainsArrivalAirport && mapContainsDepartureAirport) {
+            return map.get(arrivalAirport) && map.get(departureAirport);
         } else {
-
-            if (map.containsKey(flight.getDepartureAirport())) {
-                message += flight.getArrivalAirport();
-            } else message += flight.getDepartureAirport();
-
+            if (!mapContainsArrivalAirport && !mapContainsDepartureAirport) {
+                message = "there are no airports in " + departureAirport + " and " + arrivalAirport;
+            } else if (!mapContainsArrivalAirport) {
+                message += arrivalAirport;
+            } else {
+                message += departureAirport;
+            }
             throw new RouteNotFoundException(message);
         }
     }
@@ -39,30 +47,47 @@ public class FlightRunner {
         Flight flight1 = new Flight("Warsaw", "Prague");
         Flight flight2 = new Flight("Warsaw", "Paris");
         Flight flight3 = new Flight("Warsaw", "Machu Picchu");
+        Flight flight4 = new Flight("Szczebrzeszyn", "Warsaw");
+        Flight flight5 = new Flight("Szczebrzeszyn", "Machu Picchu");
         FlightRunner flightRunner = new FlightRunner();
 
         System.out.println("Flights availability:");
 
         try {
             System.out.print("Warsaw - Prague: ");
-            System.out.println(flightRunner.findFlght(flight1));
+            System.out.println(flightRunner.findFlight(flight1));
         } catch (RouteNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
         try {
             System.out.print("Warsaw - Paris: ");
-            System.out.println(flightRunner.findFlght(flight2));
+            System.out.println(flightRunner.findFlight(flight2));
         } catch (RouteNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
         try {
             System.out.print("Warsaw - Machu Picchu: ");
-            System.out.println(flightRunner.findFlght(flight3));
+            System.out.println(flightRunner.findFlight(flight3));
         } catch (RouteNotFoundException e) {
             System.out.println(e.getMessage());
         }
+
+        try {
+            System.out.print("Szczebrzeszyn - Warsaw: ");
+            System.out.println(flightRunner.findFlight(flight4));
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.print("Szczebrzeszyn - Machu Picchu: ");
+            System.out.println(flightRunner.findFlight(flight5));
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
