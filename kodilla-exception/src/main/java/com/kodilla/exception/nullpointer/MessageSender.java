52676de9d0bfa50d2fa539a6434cf179081eff46
urbanex <urbanex89@gmail.com>
2017-10-13 16:07:41 +0200

    moduł 8.4 - dodanie brakujacego w kodilli elsa

--- kodilla-exception/src/main/java/com/kodilla/exception/nullpointer/MessageSender.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/nullpointer/MessageSender.java
@@ -4,7 +4,8 @@ public class MessageSender {
     public void sendMessageTo(User user, String message) throws MessageNotSentException {
         if(user != null) {
             System.out.println("Sending message: " + message + " to: " + user.getName());
-        } //else
-        throw new MessageNotSentException("Object User was null");
+        } else {
+            throw new MessageNotSentException("Object User was null");
+        }
     }
 }
