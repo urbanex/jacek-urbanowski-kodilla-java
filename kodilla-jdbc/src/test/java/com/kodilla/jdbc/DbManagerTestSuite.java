d461b2ada52ef8f47c2ee0aefb6606bbbd027e08
urbanex <urbanex89@gmail.com>
2017-10-29 02:20:50 +0200

    moduł 12.6 - zadanie podejscie 1

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,65 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbManagerTestSuite {
+    @Test
+    public void testConnect() throws SQLException {
+        //Given
+        //When
+        DbManager dbManager = DbManager.getInstance();
+        //Then
+        Assert.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(5, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER" +
+                " FROM POSTS P, USERS U" +
+                " WHERE P.USER_ID = U.ID" +
+                " GROUP BY P.USER_ID" +
+                " HAVING COUNT(*) > 1;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + " " + rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(2, counter);
+    }
+}
