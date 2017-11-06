a2f29362836d368bd11f6e90b418ca084edcad5e
urbanex <urbanex89@gmail.com>
2017-11-06 23:09:32 +0100

    moduł 13.3 - zadanie podejscie nieudane

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Item.java
@@ -0,0 +1,88 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ITEMS")
+public class Item {
+    private int id;
+    private Product product;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal value;
+    private Invoice invoice;
+
+    public Item() {
+    }
+
+    public Item(Product product, BigDecimal price, int quantity) {
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.value = price.multiply(new BigDecimal(quantity));
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ITEM_ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @NotNull
+    @Column(name = "PRODUCT_PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @NotNull
+    @Column(name = "PRODUCT_QUANTITY")
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @NotNull
+    @Column(name = "ITEM_VALUE")
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "INVOICE_ID")
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,83 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    ProductDao productDao;
+    @Autowired
+    ItemDao itemDao;
+    @Autowired
+    InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave(){
+        //given
+        Product product1 = new Product("TV");
+        Product product2 = new Product("PLAYSTATION");
+        Product product3 = new Product("SPEAKERS");
+
+        BigDecimal priceForProduct1 = new BigDecimal("1500");
+        BigDecimal priceForProduct2 = new BigDecimal("2500");
+        BigDecimal priceForProduct3 = new BigDecimal("300");
+
+        Item item1 = new Item(product1, priceForProduct1, 4);
+        Item item2 = new Item(product2, priceForProduct2, 1);
+        Item item3 = new Item(product3, priceForProduct3, 8);
+
+
+        Invoice invoice1 = new Invoice("abc123");
+        invoice1.setItems(Arrays.asList(item1, item2, item3));
+
+        //when
+        productDao.save(product1);
+        int product1Id = product1.getId();
+        productDao.save(product2);
+        int product2Id = product2.getId();
+        productDao.save(product3);
+        int product3Id = product3.getId();
+
+        itemDao.save(item1);
+        int item1Id = item1.getId();
+        itemDao.save(item2);
+        int item2Id = item2.getId();
+        itemDao.save(item3);
+        int item3Id = item3.getId();
+
+        invoiceDao.save(invoice1);
+        int invoice1Id = invoice1.getId();
+
+        //then
+        Assert.assertNotEquals(0, product1Id);
+        Assert.assertNotEquals(0, product2Id);
+        Assert.assertNotEquals(0, product3Id);
+
+        Assert.assertNotEquals(0, item1Id);
+        Assert.assertNotEquals(0, item2Id);
+        Assert.assertNotEquals(0, item3Id);
+
+        Assert.assertNotEquals(0, invoice1Id);
+
+        //cleanup
+        try {
+            productDao.deleteAll();
+            itemDao.deleteAll();
+            invoiceDao.deleteAll();
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+}
