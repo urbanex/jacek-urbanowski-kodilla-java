dac25139949f5947764a328548290110cc7fcef1
urbanex <urbanex89@gmail.com>
2018-01-13 23:03:04 +0100

    modul 23.4

--- kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
@@ -8,6 +8,18 @@ public class WebDriverConfig {
     public static final String FIREFOX = "FIREFOX_DRIVER";
     public static final String CHROME = "CHROME_DRIVER";
 
+    private static String adminTrelloLogin = "";
+
+    private static String adminTrelloPassword = "mojadupa20";
+
+    public static String getAdminTrelloLogin() {
+        return adminTrelloLogin;
+    }
+
+    public static String getAdminTrelloPassword() {
+        return adminTrelloPassword;
+    }
+
     public static WebDriver getDriver(final String driver) {
         System.setProperty("webdriver.gecko.driver","c:\\Selenium-drivers\\Firefox\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver","c:\\Selenium-drivers\\Chrome\\chromedriver.exe");
