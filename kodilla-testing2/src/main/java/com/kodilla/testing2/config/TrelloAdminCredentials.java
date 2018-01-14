286d2d483666b41a05b163c2da459e6dca45f895
urbanex <urbanex89@gmail.com>
2018-01-14 23:37:47 +0100

    reszta kodu ze zmienionymi danymi logowania

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/config/TrelloAdminCredentials.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing2.config;
+
+public class TrelloAdminCredentials {
+    private static String login = "login";
+    private static String password = "haslo";
+
+    public static String getLogin() {
+        return login;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
